84227c249b1883051e951f0f8603e5332b5840ce
John Whiles <johnwhiles@outlook.com>
2016-08-12 18:16:26 +0100

    update again, not not done

---
+++ hot-damn-kata.js
@@ -0,0 +1,10 @@
+var animals = ['g','c','h','c','g','m']
+
+function boatLoader(animals) {
+    var obj = {}, result = [];
+    animals = animals.sort();
+    console.log(animals)
+    
+    animals.forEach
+    
+}
